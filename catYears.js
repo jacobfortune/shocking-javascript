d8258defa1abdcb5583c15f4ca02bcc132cebc45
Pyrobyte <132057702+ThetruePyrobyte@users.noreply.github.com>
2023-06-05 18:54:00 +0100

    Create catYears.js
    just added some more code for javascript ES6

---
+++ catYears.js
@@ -0,0 +1,9 @@
+let myAge = 'insert number here'
+myAge -= 2
+let firstYear = 1
+let secondYear = 1
+firstYear *= 15
+secondYear *= 9
+let catYear = secondYear + firstYear
+let catYears = myAge * 4 + catYear
+console.log(`I am ${myAge + 2} years old. That means in cat years I am ${catYears} years old.`)
