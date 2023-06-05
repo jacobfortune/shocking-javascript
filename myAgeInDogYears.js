a0f7f2198ec5ae9d11ff01ca35e03c5197f7ea06
Pyrobyte <132057702+ThetruePyrobyte@users.noreply.github.com>
2023-06-05 17:14:16 +0100

    Create myAgeInDogYears.js
    Simply added code.

---
+++ myAgeInDogYears.js
@@ -0,0 +1,16 @@
+let myAge = 'Insert age here'; 
+// This code sets the variable myAge to 13 //
+let earlyYears = 2;
+// This code sets the variable earlyYears to 2 //
+earlyYears *= 10.5; 
+// This multiplies the variable of earlyYears by 10.5 // 
+laterYears = myAge - 2;
+// This line of code gives the variable laterYears a value of myAge - 2 so 11 // 
+laterYears *= 4;
+// This multiplies the laterYears variable by 4 // 
+let myAgeInDogYears = laterYears + earlyYears; 
+// This code creates a new variable, myAgeInDogYears and makes it value equal to the addition of laterYears and earlyYears // 
+myName = 'Pyrobyte'.toLowerCase(); 
+// This assigns the value of the variable to the string in lower case; 'Pyrobyte'. // console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
+
+// Fill in 'insert age here' with your age and see how old you are on dog years! Do this on a proper site that releases a javascript output. //
