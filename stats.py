98a805052013df6a8e617e534c14d10d1d40352b
Jacob Fortune <Pyro.Byte@outlook.com>
2024-04-25 19:51:23 +0100

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,17 @@
+# Let's do some multiple assignment in Python
+
+# name = "Jacob"
+# age = 14
+# attractive = True
+
+name, age, attractive = "Jacob", 14, True
+strength = wisdom = constitution = dexterity = 10
+
+print("Name: "+str(name))
+print("Age: "+str(age))
+print("Attractive: "+str(attractive))
+print("Strength is "+str(strength))
+print("Wisdom is "+str(wisdom))
+print("Constitution is "+str(constitution))
+print("Dexterity is "+str(dexterity))
+
